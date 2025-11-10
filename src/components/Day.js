5b1da86b8a78b20eee29031e4fd15b83fe9d6a19
AshLaw96 <ash.l.96@outlook.com>
2025-11-10 15:08:26 +0000

    Update day with 25th day content & custom styling

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import doorImage from "../assets/images/door.jpg";
 import "../styles/Day.css";
 
 const Day = ({ day, content, isOpened, onClick }) => {
   return (
-    <Card className={`day ${isOpened ? "opened" : ""}`} onClick={onClick}>
+    <Card
+      className={`day ${isOpened ? "opened" : ""} ${
+        day === 25 ? "day-25" : ""
+      }`} // Apply 'day-25' class for Day 25
+      onClick={onClick}
+      style={{
+        backgroundImage:
+          day === 25 ? "none" : isOpened ? "none" : `url(${doorImage})`, // Remove image for Day 25
+      }}
+    >
       <Card.Body className="d-flex justify-content-center align-items-center">
-        <Card.Title className="day-number">{day}</Card.Title>
+        <Card.Title
+          className={`day-number festive-text p-2 ${
+            day === 25 ? "day-25" : ""
+          }`}
+        >
+          {day}
+        </Card.Title>
       </Card.Body>
     </Card>
   );
